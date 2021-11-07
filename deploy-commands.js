ae1b45c0c00b44131704b35af1d3d647513c6a92
Ying-Ting Chen <tingy0314@gmail.com>
2021-11-07 17:42:13 -0600

    added server info response

--- deploy-commands.js
+++ deploy-commands.js
@@ -13,6 +13,6 @@ const commands = [
 
 const rest = new REST({ version: '9' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientID, guildId), { body: commands })
-	.then(() => console.log('successfully registered application commands.'))
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
