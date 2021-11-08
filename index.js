5ef9a28df6a250c7487c0247ffb9fb6bb41d61d7
Ying-Ting Chen <tingy0314@gmail.com>
2021-11-07 19:07:11 -0600

    reconciled spacing

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ client.on('interactionCreate', async interaction => {
 
 	const { commandName } = interaction; 
 
-	if (commandName ==='ping'){
+	if (commandName === 'ping') {
 		await interaction.reply('Pong!');
 	} else if (commandName === 'server') {
 		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
 	} else if (commandName === 'user') {
-		await interaction.reply('User info.');
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
 	}
 });
 
