f5be472b5679ba3367aa8ac24dcf800851f75436
Ying-Ting Chen <tingy0314@gmail.com>
2021-12-22 14:26:15 -0600

    added test replacement text

--- commands/ping.js
+++ commands/ping.js
@@ -3,8 +3,8 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Replies with Pong!'),
+        .setDescription('Replies with condie.jpg!'),
     async execute(interaction) {
-        await interaction.reply('Pong!');
+        await interaction.reply(':condie:');
     },
 };
