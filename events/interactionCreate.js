f881bf5d6d808b18cea9591656a450522aedb5da
Ying-Ting Chen <tingy0314@gmail.com>
2022-04-10 17:09:48 -0500

    fixed typo in interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,4 +1,4 @@
-mobdule.exports = {
+module.exports = {
     name: 'interactionCreate',
     execute(interaction) {
         console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
