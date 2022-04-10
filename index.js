47aea18b7c17ac95aca10046fda2da2503e5b562
Ying-Ting Chen <tingy0314@gmail.com>
2022-04-10 14:56:22 -0500

    fixed interaction typo

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ client.on('interactionCreate', async interaction => {
 	if (!command) return;
 
 	try {
-		await command.execute(interacation);
+		await command.execute(interaction);
 	} catch (error) {
 		console.error(error);
 		await interaction.reply({ content: 'There was an error while executing this command', ephemeral: true });
