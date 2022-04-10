72554a65842b05b0267dd937fecf04fb1e198f8f
Ying-Ting Chen <tingy0314@gmail.com>
2022-04-10 14:46:17 -0500

    derefenced command object

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ client.once('ready', () => {
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
-	const { command } = client.commands.get(interaction.commandName); 
+	const command = client.commands.get(interaction.commandName); 
 
 	if (!command) return;
 
