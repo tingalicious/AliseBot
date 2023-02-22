1b0969ad20c8cb22bc19f4b2b560bc938c33696a
Ying-Ting Chen <Ying@Ying-Tings-MacBook-Pro.local>
2023-02-22 16:50:29 -0600

    fixed command and event handling

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,6 +1,24 @@
+const { Events } = require('discord.js');
+
 module.exports = {
-    name: 'interactionCreate',
-    execute(interaction) {
+    name: Events.InteractionCreate,
+    async execute(interaction) {
+        if (!interaction.isChatInputCommand()) return;
+
+        const command = interaction.client.commands.get(interaction.commandName);
+
+        if(!command) {
+            console.error(`No command matching ${interaction.commandName} was found.`);
+            return;
+        }
+
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(`Error executing ${interaction.commandName}`);
+            console.error(error);
+        }
+
         console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
     },
 };
